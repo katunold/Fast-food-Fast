45904490ad224f87b647cfe41f19278c234ddcfe
katunold <katunold94@gmail.com>
2018-09-12 16:50:27 +0300

    - Add route to get all orders [160390038]

--- api/links.py
+++ api/links.py
@@ -17,3 +17,5 @@ class Links:
         """
         app.add_url_rule('/api/v1/orders/', view_func=OrdersController.as_view('make_order'),
                          methods=['POST'], strict_slashes=False)
+        app.add_url_rule('/api/v1/orders/', view_func=OrdersController.as_view('get_orders'),
+                         methods=['GET'], strict_slashes=False)
