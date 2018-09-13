450243604f99151b2e9d7706e12ba61480bdfe84
katunold <katunold94@gmail.com>
2018-09-13 05:04:00 +0300

    - Add route for get a specific order [160390315]

--- api/links.py
+++ api/links.py
@@ -19,3 +19,5 @@ class Links:
                          methods=['POST'], strict_slashes=False)
         app.add_url_rule('/api/v1/orders/', view_func=OrdersController.as_view('get_orders'),
                          methods=['GET'], strict_slashes=False)
+        app.add_url_rule('/api/v1/orders/<int:order_id>/', view_func=OrdersController.as_view('get_single_order'),
+                         methods=['GET'], strict_slashes=False)
